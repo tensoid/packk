79239490cdf0c47c02558d4c5e3346f18a1f3de2
tensoid <iubaqua0@gmail.com>
2022-03-13 10:50:49 +0100

    added more error checking

--- bin/packk.js
+++ bin/packk.js
@@ -21,7 +21,56 @@ function printUsage(){
     console.log("\n");
 }
 
+function checkPackkFileValidity(filepath){
+
+  let json;
+
+  // Check if path exists
+  if(!fs.existsSync(filepath)){
+    console.error("File " + filepath + " does not exist");
+    process.exit(1);
+  }
+
+  // Check if is file and not directory
+  if(!fs.statSync(filepath).isFile()){
+    console.error(filepath + " is not a file");
+    process.exit(1);
+  }
+
+  //check if valid json file
+  try{
+    json = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  }catch(e){
+    console.error(filepath + " is not a valid JSON file");
+    process.exit(1);
+  }
+
+  // Check if file is a valid packk.json file
+  if(typeof json.output != "string"){
+    console.error(filepath + " is not a valid packk file");
+    console.error("Reason: output property must be a string");
+    process.exit(1);
+  }
+  else if(!Array.isArray(json.files)){
+    console.error(filepath + " is not a valid packk file");
+    console.error("Reason: files property must be a string array");
+    process.exit(1);
+  }
+  else if(typeof json.prefix != "string"){
+    console.error(filepath + " is not a valid packk file");
+    console.error("Reason: prefix property must be a string");
+    process.exit(1);
+  }
+  else if(typeof json.anonamyze != "boolean"){
+    console.error(filepath + " is not a valid packk file");
+    console.error("Reason: anonamyze property must be a boolean");
+    process.exit(1);
+  }
+}
+
 function runPackk(filepath){
+
+  checkPackkFileValidity(filepath);
   
   const packkConfig = JSON.parse(fs.readFileSync(filepath, 'utf8'));
 
@@ -43,6 +92,11 @@ function runPackk(filepath){
   let code = {};
 
   packkConfig.files.forEach(file => {
+    if(typeof file != "string"){
+      console.error("'" + file + "' is not a valid file");
+      console.error("Consider checking the files property in your packk.json");
+      process.exit(1);
+    }
     code[file] = fs.readFileSync(file).toString();
   });
 
